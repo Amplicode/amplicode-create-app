c32cb004607846860f6f0807fb48f3230403be4e
Artyom Safronov <a.safronov@haulmont.net>
2024-04-23 16:11:49 +0400

    fix initial app

--- test-ra-app/src/dataProvider.ts
+++ test-ra-app/src/dataProvider.ts
@@ -5,9 +5,7 @@ const baseDataProvider = springDataProvider(
   import.meta.env.VITE_SIMPLE_REST_URL
 );
 
-export interface CustomDataProvider extends DataProvider {
-
-}
+export interface CustomDataProvider extends DataProvider {}
 
 export const dataProvider: CustomDataProvider = {
   ...baseDataProvider,
